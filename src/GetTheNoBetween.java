1725fb2ca493946acbf1b149eeae9b7a0cca7aa1
Dharani 007 <dharanimeenatchisundharam112@gmail.com>
2023-02-24 20:02:36 +0530

    Added : Get the number using random

---
+++ src/GetTheNoBetween.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class GetTheNoBetween {
+    public static void main(String[] args) {
+        Random rc = new Random();
+        int randomNumber = (int) (Math.random() * 6) + 1;
+
+
+        System.out.println("The number is " + randomNumber);
+    }
+}
+
+
