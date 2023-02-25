0e1b8e9412e4a32fb87ce468ece82c969fc5d0ae
Dharani 007 <dharanimeenatchisundharam112@gmail.com>
2023-02-25 07:48:37 +0530

    Added : They are No Play,Ladder or Snake.

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -2,5 +2,25 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Game");
         int startPos = 0;
+        int random = (int) (Math.random() * 10);
+        int dice = (random % 6) + 1;
+        int option = random % 3;
+        int value = random % 6;
+
+        int i = 0;
+        int noplay = 0;
+        int ladder = 1;
+        int snake = 2;
+        if (dice == noplay) {
+            System.out.println("No Play" + i);
+        } else if (option == ladder) {
+            i = i + value;
+            System.out.println("Ladder" + i);
+        } else {
+            i = i - value;
+            System.out.println("snake" + i);
+        }
+        System.out.println("player rolls die and get value=" + i);
+
     }
 }
