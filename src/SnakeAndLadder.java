6ecb13ab74e0cabe12f63b4b8caa66cb0d1c7a0f
Dharani 007 <dharanimeenatchisundharam112@gmail.com>
2023-02-25 07:52:37 +0530

    Added : Repeat till the Player reaches the winning position 100.

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -11,16 +11,29 @@ public class SnakeAndLadder {
         int noplay = 0;
         int ladder = 1;
         int snake = 2;
-        if (dice == noplay) {
-            System.out.println("No Play" + i);
-        } else if (option == ladder) {
-            i = i + value;
-            System.out.println("Ladder" + i);
-        } else {
-            i = i - value;
-            System.out.println("snake" + i);
-        }
-        System.out.println("player rolls die and get value=" + i);
+        while (i <= 20)
+            while (i <= 100) {
+                if (dice == noplay) {
+                    System.out.println("No Play" + i);
+                } else if (option == ladder) {
+                    i = i + value;
+                    System.out.println("Ladder" + i);
+                } else {
+                    i = i - value;
+                    System.out.println("snake" + i);
+                }
+                //- Note In case the player position moves
+                //below 0, then the player restarts from 0
+                i = i - value;
+                if (i < 0) {
+                    i = 0;
+                    System.out.println("snake" + i);
+                } else {
+                    System.out.println("snake" + i);
+                }
+                System.out.println("player rolls die and get value=" + i);
+                System.out.println("Dice Roll =" + value);
+            }
 
     }
 }
