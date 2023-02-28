52eb74bc3f677ed4fbc3f2546c69b924211bb723
Dharani 007 <dharanimeenatchisundharam112@gmail.com>
2023-02-28 19:20:47 +0530

    Added : Get the number between 1 to 6

--- src/GetTheNoBetween1to6.java
+++ src/GetTheNoBetween1to6.java
@@ -1,12 +1,16 @@
 import java.util.Random;
 
-public class GetTheNoBetween {
+public class GetTheNoBetween1to6 {
     public static void main(String[] args) {
+        int postion = 0;
         Random rc = new Random();
         int randomNumber = (int) (Math.random() * 6) + 1;
 
         System.out.println("The number is " + randomNumber);
     }
 }
+
+
+
 
 
